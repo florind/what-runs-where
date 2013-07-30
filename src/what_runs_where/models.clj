8d39c34350d0979b6e844951c0af95805a32a9e4
Florin Duroiu <anotherflo@yahoo.com>
2013-07-30 11:16:32 +0200

    Upgraded to bootstrap 3.0. Replaced local bootstrap css and js with CDN resources.

--- src/what_runs_where/models.clj
+++ src/what_runs_where/models.clj
@@ -14,8 +14,8 @@ Example return {:backend \"1.1\"}"
           req-time (get response :request-time),
         ver-str (re-find regex body)]
       (case status
-        200 (if (nil? ver-str) {service-key :garble} {service-key (str (last ver-str) " " req-time " ms")})
-        {service-key (str "error: server status " (str status " " req-time " ms"))}))
+        200 (if (nil? ver-str) {service-key :garble} {service-key (last ver-str)})
+        {service-key (str "error: server status " status)}))
     (catch Exception e {service-key (str "error: " e)})))
 
 (defn services-ver-map
--- src/what_runs_where/views.clj
+++ src/what_runs_where/views.clj
@@ -6,34 +6,32 @@
 
 (defn server-casette [status]
   (let [srv-name (key status)
-        srv-html-name (str "<a href=" srv-name " style='color: white'>"
+        srv-html-name (str "<a href=" srv-name " style='color: white;white-space:pre-wrap'>"
                              (if (> (count srv-name) 24) (str (subs srv-name 0 23) "<br/>" (subs srv-name 23)) srv-name) "</a>")
         stats (vals (val status))]
     (html
-	    [:div.span3 
+	    [:div {:class "col-lg-2 col-sm-4 col-12"}
 	     [:div.well.well-small
         [:center
          (if (some #((complement nil?) (re-find #"^error" %)) stats) 
-           (if (every? #((complement nil?) (re-find #"^error" %)) stats) [:span {:class "label label-important"} srv-html-name]
+           (if (every? #((complement nil?) (re-find #"^error" %)) stats) [:span {:class "label label-danger"} srv-html-name]
              [:span {:class "label label-warning"} srv-html-name])
          [:span {:class "label label-success"} srv-html-name])]
 	      [:hr.horline]
        (map 
-         #(html [:small [:span (str (key %) ": " (val %))]] [:br]) (val status))
+         #(html [:small [:span3 (str (key %) ": " (val %))]] [:br]) (val status))
       ]])))
 
 (defn env-rows [srv-list]
-  (html [:div.row 
 	(map 
-    (fn [%] (html [:div.row-fluid 
+    (fn [%] (html [:div.row
       (map 
-        #(server-casette %) %)])) (partition-all 4 srv-list))]))
+        #(server-casette %) %)])) (partition-all 6 srv-list)))
 
 (defn header []
    (html [:html 
           [:head 
-           (include-css "/css/bootstrap.min.css") 
-           (include-css "/css/bootstrap-responsive.css") 
+           (include-css "//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css") 
            (include-css "/css/site.css")]]))  
 
 (defn footer []
@@ -41,7 +39,7 @@
             [:div.container
              [:p.muted.credit (html "&copy; 2013&nbsp;&nbsp;&nbsp;" [:a {:href "/help"} "Help"])]]]
         (include-js "http://code.jquery.com/jquery-latest.js")
-        (include-js "/js/bootstrap.js")))
+        (include-js "//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js")))
 
 (defn overview-page [server-map fun]
   (let [srv-lst (flatten (map #(:servers %) (vals server-map)))]
@@ -52,13 +50,13 @@
 		     [:div.container
 		      [:div.page-header [:h1 [:center "What Runs Where?"]]]
 	        [:div.row 
-	         [:div.span12 {:align "center"}
+	         [:div.row {:align "center"}
 	          [:strong 
-	           [:span.badge.badge-info (count srv-lst) " service(s)"] "  On " (clj-time.local/format-local-time (clj-time.local/local-now) :rfc822)]]]
+	           [:span {:class "label label-info"} (count srv-lst) " service(s)"] "  On " (clj-time.local/format-local-time (clj-time.local/local-now) :rfc822)]]]
           (pmap 
             #(html
                [:hr.horline]
-               [:div.span12 [:h5 (key %)]]
+               [:div.row [:h5 (key %)]]
                (env-rows (fun (val %)))) server-map)]
        [:div#push]]
 	      (footer)
