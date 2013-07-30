033f3fb2e5af4bcea0b74484d485bdd8bbb80470
Florin Duroiu <anotherflo@yahoo.com>
2013-07-30 13:24:19 +0200

    Test fix, one more added

--- test/what_runs_where/test/handler.clj
+++ test/what_runs_where/test/handler.clj
@@ -1,22 +1,34 @@
 (ns what-runs-where.test.handler
   (:use clojure.test
         ring.mock.request  
-        what-runs-where.handler))
+        what-runs-where.handler
+        ring.adapter.jetty))
 
 ;first load the test config
 (load-config "test/what_runs_where/test/config-test.properties")
 
 (deftest test-app
+  (testing "/version URL"
+     ;start web server
+     (def srv (what-runs-where.handler/-main "-c" "test/what_runs_where/test/config-test.properties" "-p" "40008"))
+    (let [api-response (app (request :get "/api/services"))
+          clojure-ver (last (re-find #"\"Clojure\":\"([\d.]*)\"" (:body api-response)))]
+      (is (= clojure-ver (clojure-version))))
+    ;stop web server
+    (.stop srv)
+    )
+  
   (testing "main route"
     (let [response (app (request :get "/"))
           service-count (re-find #"2 service\(s\)" (:body response))]
       (is (= (:status response) 200))
       (is ((complement nil?) service-count))))
-  (testing "api"
-    (let [api-response (app (request :get "/api/services"))
-          clojure-ver (re-find #"\"Clojure\":\"([\d.]*)\"" (:body api-response))]
-      (is (= clojure-ver (clojure-version)))))
-  
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      ))
+  (testing "/version URL"
+    (let [api-response (app (request :get "/version"))
+          clojure-ver (last (re-find #"\"clojure\": \"([\d.]*)\"" (:body api-response)))]
+      (is (= (:status api-response) 200))
+        (is (= clojure-ver (clojure-version))))))
